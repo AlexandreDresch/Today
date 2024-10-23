17abbad586e924b5aa09599693f0029b57ad46bd
AlexandreDresch <connectWithAlexandre@proton.me>
2024-10-22 22:49:12 -0300

    chore: add initial config

---
+++ src/components/component.ts
@@ -0,0 +1,24 @@
+import { ComponentProps } from "../types";
+
+export class Component {
+  props: ComponentProps;
+  element: DocumentFragment;
+
+  constructor(props: ComponentProps = {}) {
+    this.props = props;
+    this.element = document.createDocumentFragment();
+  }
+
+  render(): string {
+    return "";
+  }
+
+  mount(target: HTMLElement): void {
+    const tempDiv = document.createElement("div");
+    tempDiv.innerHTML = this.render();
+    while (tempDiv.firstChild) {
+      this.element.appendChild(tempDiv.firstChild);
+    }
+    target.appendChild(this.element);
+  }
+}
