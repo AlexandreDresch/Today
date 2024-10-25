81a516366d63876070c19bd35e425df2a4d08cce
AlexandreDresch <connectWithAlexandre@proton.me>
2024-10-24 23:57:22 -0300

    style: add fixed position to Header component

--- src/components/header.ts
+++ src/components/header.ts
@@ -3,7 +3,7 @@ import { Component } from "./shared/component.ts";
 export class Header extends Component {
   render(): string {
     return /*html*/ `
-      <header class="flex items-center justify-between bg-white border border-gray-300 h-16">
+      <header class="fixed w-full top-0 left-0 flex items-center justify-between bg-white border border-gray-300 h-16">
         <nav aria-label="Main navigation" class="pl-10">
           <a href="/" class="font-orbitron font-bold text-xl antialiased"  aria-label="Today - Home">
             Today
