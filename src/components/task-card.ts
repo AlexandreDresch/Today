2c29b8f8618003399e8a5947080bcf46e2a1c1ac
AlexandreDresch <connectWithAlexandre@proton.me>
2024-11-05 00:20:14 -0300

    feat: add checkbox & popover to task description

--- src/components/task-card.ts
+++ src/components/task-card.ts
@@ -13,14 +13,19 @@ export class TaskCard extends Component {
       <div class="flex justify-between p-2 bg-gray-100">
           <div class="flex flex-col space-y-3">
               <h3>${title}</h3>
-              <p>${description}</p>
+              <p 
+                class="max-w-36 sm:max-w-full truncate" 
+                title="${description}"
+              >
+                ${description}
+              </p>
               <p>Category: ${category}</p>
           </div>
   
           <div class="flex flex-col justify-between">
-              <img src="/icons/circle-check-big.svg"/>
-              <img src="/icons/square-pen.svg"/>
-              <img src="/icons/trash-2.svg"/>
+              <input id="checkbox" type="checkbox" value="" class="size-[14px] bg-transparent border-gray-300 focus:ring-black focus:ring-1 rounded-none">
+              <img src="/icons/square-pen.svg" class="size-4"/>
+              <img src="/icons/trash-2.svg" class="size-4"/>
           </div>
       </div>
       `;
