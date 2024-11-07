295a1d671516e2d9ea4155794ea013e82ea709e6
AlexandreDresch <connectWithAlexandre@proton.me>
2024-11-06 23:51:09 -0300

    feat: add id & completed props to TaskCard component

--- src/components/task-card.ts
+++ src/components/task-card.ts
@@ -7,10 +7,10 @@ export class TaskCard extends Component {
   }
 
   render(): string {
-    const { title, description, category } = this.props as Task;
+    const { id, title, description, category, completed } = this.props as Task;
 
     return /*html*/ `
-      <div class="flex justify-between p-2 bg-gray-100">
+      <div class="flex justify-between p-2 bg-white border border-black shadow-[rgba(6,_24,_44,_0.4)_0px_0px_0px_2px,_rgba(6,_24,_44,_0.65)_0px_4px_6px_-1px,_rgba(255,_255,_255,_0.08)_0px_1px_0px_inset]" key="${id}">
           <div class="flex flex-col space-y-3">
               <h3>${title}</h3>
               <p 
@@ -23,7 +23,7 @@ export class TaskCard extends Component {
           </div>
   
           <div class="flex flex-col justify-between">
-              <input id="checkbox" type="checkbox" value="" class="size-[14px] bg-transparent border-gray-300 focus:ring-black focus:ring-1 rounded-none">
+              <input id="checkbox" type="checkbox" value="${completed}" class="size-[14px] bg-transparent border-gray-300 focus:ring-black focus:ring-1 rounded-none">
               <img src="/icons/square-pen.svg" class="size-4"/>
               <img src="/icons/trash-2.svg" class="size-4"/>
           </div>
