af9841a160b9d59175818bbb0f46db7e7a93dfbe
AlexandreDresch <connectWithAlexandre@proton.me>
2024-11-12 22:22:45 -0300

    style: update style of action buttons

--- src/components/delete-task-modal.ts
+++ src/components/delete-task-modal.ts
@@ -44,14 +44,14 @@ export class DeleteTaskModal extends Component {
           <div class="flex justify-end space-x-2 mt-4">
             <button 
               id="cancelButton" 
-              class="px-4 py-2 border border-gray-300 bg-white hover:bg-gray-100"
+              class="bg-white text-black px-8 py-2 font-archivo font-semibold tracking-widest border-2 border-b-4 border-l-4 border-black hover:border-t-4 hover:border-b-2"
               aria-label="Cancel task deletion"
             >
               Cancel
             </button>
             <button 
               id="confirmButton" 
-              class="px-4 py-2 border border-red-600 bg-red-600 text-white hover:bg-red-700"
+              class="bg-white text-red-600 px-8 py-2 font-archivo font-semibold tracking-widest border-2 border-b-4 border-l-4 border-red-600 hover:border-t-4 hover:border-b-2"
               aria-label="Confirm task deletion"
             >
               Delete
